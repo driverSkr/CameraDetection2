87909f27398fd3f3df56ab05c918e38405212aa7
zoucongbo <2237913536@qq.com>
2026-03-05 18:13:50 +0800

    Wifi检测

--- app/src/main/java/com/ethan/cameradetection2/ui/wifi/page/WiFiCamerasPage.kt
+++ app/src/main/java/com/ethan/cameradetection2/ui/wifi/page/WiFiCamerasPage.kt
@@ -1,8 +1,57 @@
 package com.ethan.cameradetection2.ui.wifi.page
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.ethan.cameradetection2.R
+import com.ethan.cameradetection2.ui.wifi.view.RadarScannerWithControls2
+import com.ethan.cameradetection2.utils.findBaseActivityVBind
 
 @Composable
 fun WiFiCamerasPage() {
+    val context = LocalContext.current
+    val isAnimating = remember { mutableStateOf(false) }
 
+    LaunchedEffect(Unit) {
+        isAnimating.value = true
+    }
+
+    Column(modifier = Modifier.fillMaxSize().statusBarsPadding()) {
+        Box(modifier = Modifier.fillMaxWidth().padding(start = 15.dp, top = 9.dp)) {
+            Image(painter = painterResource(R.drawable.svg_back), contentDescription = null, modifier = Modifier.align(Alignment.CenterStart).clickable{
+                context.findBaseActivityVBind()?.finish()
+            })
+            Text("WiFi Cameras", color = Color(0xFF152946), fontSize = 18.sp, fontWeight = FontWeight.Bold, modifier = Modifier.align(Alignment.Center))
+        }
+
+        Spacer(modifier = Modifier.height(20.dp))
+        Box(modifier = Modifier.fillMaxWidth().height(360.dp)) {
+            RadarScannerWithControls2(isAnimating)
+            Row(modifier = Modifier.align(Alignment.BottomCenter), verticalAlignment = Alignment.CenterVertically) {
+                Text("Found Devices:", color = Color(0xFF152946), fontSize = 16.sp)
+                Text("99", color = Color(0xFF152946), fontSize = 18.sp, fontWeight = FontWeight.Bold)
+            }
+        }
+    }
 }
