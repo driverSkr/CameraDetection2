067257f6c9342f148fd7fa9ddfa698a9f1caadcb
zoucongbo <2237913536@qq.com>
2026-05-12 11:20:22 +0800

    配置商品id

--- Libs/GPay/src/main/java/com/ethan/pay/utils/SubHelper.kt
+++ Libs/GPay/src/main/java/com/ethan/pay/utils/SubHelper.kt
@@ -5,14 +5,17 @@ object SubHelper {
     /**
      * 订阅商品id
      */
-    private const val product_id_sub = "hidden.spycamera.premium"
+    private const val product_id_sub = "spy.yearly"
 
     /**
      * 订阅商品plan_id
      */
-    private const val plan_id_week = "spycamera-weekly"
-    private const val plan_id_month = "spycamera-monthly"
-    private const val plan_id_year = "spycamera-yearly"
+    private const val plan_id_week = "spy-weekly-basic"
+    private const val plan_id_month = "spy-monthly-basic"
+    private const val plan_id_year = "spy-yearly-basic"
+
+    private const val offer_id_month = "spy-monthly-first-discount"
+    private const val offer_id_year = "spy-yearly-first-discount"
 
     /**
      * todo 请注意，如果添加了lifetime套餐，请向该list添加！！！，不然无法识别lifetime权益,以及积分包添加！！！！
@@ -27,20 +30,28 @@ object SubHelper {
         return plan_id_week
     }
 
+    fun getWeekSkuId(): String {
+        return product_id_sub
+    }
+
     fun getMonthPlanId(): String {
         return plan_id_month
+    }
+
+    fun getMonthOfferId(): String {
+        return offer_id_month
+    }
+
+    fun getMonthSkuId(): String {
+        return product_id_sub
     }
 
     fun getYearPlanId(): String {
         return plan_id_year
     }
 
-    fun getWeekSkuId(): String {
-        return product_id_sub
-    }
-
-    fun getMonthSkuId(): String {
-        return product_id_sub
+    fun getYearOfferId(): String {
+        return offer_id_year
     }
 
     fun getYearSkuId(): String {
